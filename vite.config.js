25799dc8e9454a75696957678a32a95bd2707f15
Mohanad20100 <mohanad.mahmoud20100@gmail.com>
2026-06-14 23:39:10 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
-  // 💡 تنبيه هام: إذا كنت ترفع المنصة على GitHub Pages، 
-  // استبدل 'My-first-project' باسم المستودع (Repository) الخاص بك تماماً كما يظهر في الرابط.
-  // أما إذا كنت ترفع على منصة Vercel، يمكنك حذف السطر التالي بالكامل.
-  base: '/My-first-project/',
+  base: './', // 👈 هذه الخاصية تضمن تشغيل مسارات الملفات بشكل سليم بعد الرفع
 })
